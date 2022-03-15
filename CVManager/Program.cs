fbfa734f7425ac4144f4b9734eb6083edfdfe43d
Bartek <Bartek@MOLDOW-PC78>
2022-03-15 20:28:26 +0100

    Add project files.

---
+++ CVManager/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Przywitanie
+// Wybór akcji
+/// 1. Stworzenie nowego kandydata
+//// A. Wybierz kategorię rekrutacji (pracownik umyslowy, fizyczny, kierowca)
+//// B. Wprowadzenie szczegółów (Imię, Nazwisko etc.)
+/// 2. Usunięcie danych kandydata
+//// A. Wskazanie id lub nazwy kandydata
+//// B. Wskazanie usunięcia CV lub całego profilu kandydata
+//// C. Usunięcie z listy
+/// 3. Sprawdzenie bazy danych kandydatów
+//// A. Wskazanie id lub nazwy kandydata
+//// B. Wyświetlenie wszystkich informacji zw. z kandydatem
+/// 4. Zwrócenie listy CV po zadanym filtrze kategorii
+//// A. Wskazanie nazwy lub kategorii
+//// B. Wyświetlenie listy 
+
+
+Console.WriteLine("Welcome to CVManager console app!");
+Console.WriteLine("Please select the operation:");
